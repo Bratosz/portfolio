a9736cf648df59686d26d03b50c3ff9957415f7e
Bartosz Palej <palej.bartosz@gmail.com>
2020-03-30 21:06:59 +0200

    ODC 4

--- js/timer.js
+++ js/timer.js
@@ -1,22 +1,18 @@
-function odliczanie()
+function countdown()
 	{
-		var dzisiaj = new Date();
+		var now	= new Date();
 		
-		var dzien = dzisiaj.getDate();
-		var miesiac = dzisiaj.getMonth()+1;
-		var rok = dzisiaj.getFullYear();
+		var hour = now.getHours();
+		if (hour<10) hour = "0" + hour;
 		
-		var godzina = dzisiaj.getHours();
-		if (godzina<10) godzina = "0"+godzina;
+		var minute = now.getMinutes();
+		if (minute<10) minute = "0" + minute;
 		
-		var minuta = dzisiaj.getMinutes();
-		if (minuta<10) minuta = "0"+minuta;
+		var second = now.getSeconds();
+		if (second<10) second = "0" + second;
 		
-		var sekunda = dzisiaj.getSeconds();
-		if (sekunda<10) sekunda = "0"+sekunda;
-		
-		document.getElementById("zegar").innerHTML = 
-		 dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
+		document.getElementById("clock").innerHTML = 
+		 hour + ":" + minute + ":" + second;
 		 
-		 setTimeout("odliczanie()",1000);
+		 setTimeout("countdown()",1000);
 	}
